14e131b3ed7fb14807871941becc28ec74bb8bdb
Diego <dpjaraba@gmail.com>
2023-06-16 14:50:10 +0200

    borrador imc

--- basicos/src/basicos/EjercicosBasicos.java
+++ basicos/src/basicos/EjercicosBasicos.java
@@ -311,6 +311,42 @@ pista: PARA LEER DE TECLADO USAD LA CLASE SCANNER
 public static void AssessmentObesidad()
 {
  //TODO hacer el IMC
+	
+
+	
+	System.out.println("Introduce tu peso en kg");
+	
+	Scanner scanner = new Scanner (System.in);
+	
+	float peso=scanner.nextFloat();
+		
+	System.out.println(queNota(nota));
+	
+	
+	
+	
+		String cad="";
+		if(imc()<16.00){
+		  cad="Infrapeso: Delgadez Severa";
+		}else if(imc()<=16.00 || imc()<=16.99){
+		  cad="Infrapeso: Delgadez moderada";
+		}else if(imc()<=17.00 ||imc()<=18.49){
+		  cad="Infrapeso: Delgadez aceptable";
+		}else if(imc()<=18.50 || imc()<=24.99){
+		  cad="Peso Normal";
+		}else if(imc()<=25.00 || imc()<=29.99){
+		  cad="Sobrepeso";
+		}else if(imc()<=30.00 || imc()<=34.99){
+		  cad="Obeso: Tipo I";
+		}else if(imc()<=35.00 || imc()=40.00){
+		  cad="Obeso: Tipo III";
+		}else{
+		  cad="no existe clasificacion";
+		}
+		  return cad;
+	
+	
+	
 }
 }
 
